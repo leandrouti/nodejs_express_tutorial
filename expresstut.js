fa57af4d4a5e69189019ebfb3023197bbdc05846
Utiyama Tatsuya Leandro <leandrouti@yahoo.com.br>
2015-11-29 22:12:46 +0900

    Added HTML handlebars

--- expresstut.js
+++ expresstut.js
@@ -2,12 +2,53 @@ var express = require('express');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.disable('x-powered-by');
 
-app.get('/', function(req,res){
-   res.send('Express Worked');
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars',handlebars.engine);
+app.set('view engine', 'handlebars');
+
+//more imports here
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname+'/public'));
+
+app.get('/', function(req, res){
+   res.render('home');
 });
 
-app.listen(app.get('port'), function(){
-   console.log('Express started press Ctrl-C to terminate');
+app.use(function(req,res, next){
+   console.log("Looking for page"+req.url);
+   next();
+});
+
+app.get('/junk', function(req, res, next){
+   console.log("Tried to access /junk");
+   throw new Error('/junk doesn\'t exist');
+});
+
+app.use(function(err, req, res, next){
+   console.log("Error: "+err.message);
+   next();
+});
+
+app.get('/about', function(req, res){
+   res.render('about');
+});
+
+app.use(function(req, res){
+   res.type('text/html');
+   res.status(404);
+   res.render('404');
+});
+
+app.use(function(res, req){
+   res.type('text/html');
+   res.status(500);
+   res.render('500');
+})
+
+app.listen(app.set('port'), function(){
+   console.log('Express started on http://localhost: "'+app.get('port')+'press Ctrl+c to terminate');
 });
